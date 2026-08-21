876fe62cb90212464868e4391eeed6381b10c632
Nicholas Malcolm <bubylou@pm.me>
2026-08-21 19:41:43 -0400

    init

---
+++ flake.nix
@@ -0,0 +1,9 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    import-tree.url = "github:denful/import-tree";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
+}
